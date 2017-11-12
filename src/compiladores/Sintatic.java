2de5e9cbaa7ba7f306cbd6cac86bcc56fa069a4c
Lucas Correa <lucashuati@hotmail.com>
2017-11-12 17:06:19 -0200

    working decl

--- src/compiladores/Sintatic.java
+++ src/compiladores/Sintatic.java
@@ -13,94 +13,113 @@ public class Sintatic {
             System.out.println("Unexpected Token");
 	}
 	
-	boolean eat(int tag) {
-            return tag == tok.tag;
+	void eat(int tag) throws IOException {
+            if(tag == tok.tag) advance();
+            else error();
 	}
 	
-	void identList() {
-		
+        boolean identifier() throws IOException{ // true = more | false = not more
+            switch(tok.tag){
+                case Tag.ID:
+                    eat(Tag.ID);
+                    break;
+                default:
+                    error();
+            }
+            if(tok.tag == Tag.COM){
+                eat(Tag.COM);
+                return true;
+            }else {
+                return false;
+            }
+        }
+	void identList() throws IOException {
+            while(identifier());     
 	}
 	
-	void decl() {
-		switch(tok.tag) {
-			case Tag.INT: 
-				eat(Tag.INT);
-				identList();
-				eat(Tag.DOTCOM);
-				break;
-			case Tag.STRING:
-				decl(); 
-				break;
-			default: error();
-		}
-	}
-	
-	
-	void declList() { // optional
+	boolean decl() throws IOException {
+            boolean valid = false;
+            
             switch(tok.tag) {
                 case Tag.INT: 
-                    decl(); 
+                    eat(Tag.INT);
+                    identList();
+                    eat(Tag.DOTCOM);
+                    valid = true;
                     break;
 		case Tag.STRING:
-                    decl(); 
-                    break;	
-            }
+                    decl();
+                    valid = true;
+                    break;
+		}
+            return valid;
 	}
 	
-	void condition() {
-		
+	void declList() throws IOException { // optional 
+            while(decl()){};
 	}
 	
-	void stmtSufix() {
-            switch(tok.tag) {
-                case Tag.WHILE:
-                    eat(Tag.WHILE);
-                    condition();
-                    eat(Tag.END);
-		default: error();
-            }
-	}
+//	void condition() {
+//		
+//	}   
 	
-	void ifStmt() {
-            switch(tok.tag) {
-		case Tag.IF:
-                    eat(Tag.IF);
-			break;
-		default: error();
-            }
-	}
+//	void stmtSufix() {
+//            switch(tok.tag) {
+//                case Tag.WHILE:
+//                    eat(Tag.WHILE);
+//                    condition();
+//                    eat(Tag.END);
+//		default: error();
+//            }
+//	}
 	
-	void whileStmt() {
+//	void ifStmt() {
+//            switch(tok.tag) {
+//		case Tag.IF:
+//                    eat(Tag.IF);
+//			break;
+//		default: error();
+//            }
+//	}
+	
+	void whileStmt() throws IOException {
             switch(tok.tag) {
 		case Tag.DO: 
                     eat(Tag.DO);
                     stmtList();
-                    stmtSufix();
+//                    stmtSufix();
                     break;
 		default: error();
             }
 	}
 	
-	
-	void stmtList() {
-            switch(tok.tag) {
-		case Tag.IF: 
-                    ifStmt();
-                    break;
-		case Tag.DO:
-                    whileStmt(); 
-                    break;
-                default: error();
-		}
+	boolean stmt() throws IOException {
+          boolean valid = false;
+          switch(tok.tag){
+            case Tag.IF: 
+//                ifStmt();
+                valid = true;
+                break;
+            case Tag.DO:
+                whileStmt(); 
+                valid = true;
+                break;
+          } 
+          return valid;
+        }
+	void stmtList() throws IOException { //stmt {stmt}
+            while(stmt());
 	}
 	
-	void program() {
+	void program() throws IOException {
             //  program [decl-list] stmt-list end
             switch(tok.tag) {
                 case Tag.PRG: 
                     eat(Tag.PRG); 
+                    System.out.println("PRG OK");
                     declList(); // optional
-                    stmtList(); 
+                     System.out.println("Decl OK");
+                    //stmtList(); 
                     eat(Tag.END); 
                     break;
 		default: error();		
